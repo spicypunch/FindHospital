dd42dcf2df3166db03470bcc2266d5eac6cee9f5
spicypunch <72846127+spicypunch@users.noreply.github.com>
2023-06-28 10:44:17 +0900

    RXJava 적용

--- app/src/main/java/com/example/lifesemantics/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/lifesemantics/ui/home/HomeViewModel.kt
@@ -4,49 +4,44 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.lifesemantics.data.entity.HospitalInfoResponse
 import com.example.lifesemantics.repository.HospitalInfoRepositoryImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val hospitalInfoRepository: HospitalInfoRepositoryImpl
 ) : ViewModel() {
-    // 현재 페이지를 가지고 있는 LiveData
-    private var _cnt = MutableLiveData(1)
-    val cnt: LiveData<Int>
-        get() = _cnt
-
     private var _hospitalInfo = MutableLiveData<HospitalInfoResponse>()
     val hospitalInfo: LiveData<HospitalInfoResponse>
         get() = _hospitalInfo
 
-    private var hospitalName = ""
-    private var latitude = 0.0
-    private var longitude = 0.0
-    private var pageNo = 1
+    private val compositeDisposable = CompositeDisposable()
 
     fun getHospitalInfo(hospitalName: String, latitude: Double, longitude: Double) {
-        viewModelScope.launch {
-            try {
-                pageNo = 1
-                _hospitalInfo.value = hospitalInfoRepository.getHospitalInfo(
-                    hospitalName = hospitalName,
-                    pageNo = pageNo,
-                    latitude = latitude,
-                    longitude = longitude
-                )
-                this@HomeViewModel.hospitalName = hospitalName
-                this@HomeViewModel.latitude = latitude
-                this@HomeViewModel.longitude = longitude
+        val disposable = Single.fromCallable {
+            hospitalInfoRepository.getHospitalInfo(
+                hospitalName = hospitalName,
+                latitude = latitude,
+                longitude = longitude
+            )
+        }.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ response ->
+                _hospitalInfo.value = response
+            }, { error ->
+                Log.e("Error", error.toString())
+            })
+        compositeDisposable.add(disposable)
+    }
 
-                _cnt.value = pageNo
-            } catch (e: Exception) {
-                Log.e("Exception", e.toString())
-            }
-        }
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
     }
 }
