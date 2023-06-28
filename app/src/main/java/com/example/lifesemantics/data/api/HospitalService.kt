91ce3a0c2c30ebac7d40f0bb7a7a5d29cf7a8d1d
spicypunch <72846127+spicypunch@users.noreply.github.com>
2023-06-28 10:45:28 +0900

    RXJava 적용

--- app/src/main/java/com/example/lifesemantics/data/api/HospitalService.kt
+++ app/src/main/java/com/example/lifesemantics/data/api/HospitalService.kt
@@ -7,10 +7,9 @@ import retrofit2.http.Query
 interface HospitalService {
 
     @GET("/B551182/hospInfoServicev2/getHospBasisList")
-    suspend fun getHospitalInfo(
+    fun getHospitalInfo(
         @Query("ServiceKey") serviceKey: String,
         @Query("yadmNm") hospitalName: String,
-        @Query("pageNo") pageNo: String,
         @Query("yPos") latitude: String,
         @Query("xPos") longitude: String,
         @Query("radius") radius: String
