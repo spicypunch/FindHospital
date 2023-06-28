0185a75a05b60f6e116211bb88e316e0cdb1d949
spicypunch <72846127+spicypunch@users.noreply.github.com>
2023-06-28 10:44:43 +0900

    RXJava 적용

--- app/src/main/java/com/example/lifesemantics/repository/HospitalInfoRepositoryImpl.kt
+++ app/src/main/java/com/example/lifesemantics/repository/HospitalInfoRepositoryImpl.kt
@@ -8,11 +8,14 @@ class HospitalInfoRepositoryImpl @Inject constructor(
     private val hospitalService: HospitalService
 ) : HospitalInfoRepository {
 
-    override suspend fun getHospitalInfo(hospitalName: String, pageNo: Int, latitude: Double, longitude: Double): HospitalInfoResponse {
+    override fun getHospitalInfo(
+        hospitalName: String,
+        latitude: Double,
+        longitude: Double
+    ): HospitalInfoResponse {
         return hospitalService.getHospitalInfo(
             serviceKey = "Ly2IHtl1aGXioF/sk3QPO8m0vKzS0zMpHGsaq3gfvRWJ7wHg1Pim+YJW7mchXjPxvt/s1BHsszlod8Qqv8CVVA==",
             hospitalName = hospitalName,
-            pageNo = pageNo.toString(),
             latitude = latitude.toString(),
             longitude = longitude.toString(),
             radius = "5000"
