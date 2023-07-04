812b620e4338ee1a42924a317a926110c664a3ea
spicypunch <miniguni66@naver.com>
2023-07-04 23:31:25 +0900

    Paging3 라이브러리 추가

--- app/src/main/java/com/example/lifesemantics/repository/HospitalInfoRepositoryImpl.kt
+++ app/src/main/java/com/example/lifesemantics/repository/HospitalInfoRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.lifesemantics.repository
 
+import com.example.lifesemantics.BuildConfig
 import com.example.lifesemantics.data.entity.HospitalInfoResponse
 import com.example.lifesemantics.data.api.HospitalService
 import javax.inject.Inject
@@ -10,7 +11,7 @@ class HospitalInfoRepositoryImpl @Inject constructor(
 
     override suspend fun getHospitalInfo(hospitalName: String, pageNo: Int, latitude: Double, longitude: Double): HospitalInfoResponse {
         return hospitalService.getHospitalInfo(
-            serviceKey = "Ly2IHtl1aGXioF/sk3QPO8m0vKzS0zMpHGsaq3gfvRWJ7wHg1Pim+YJW7mchXjPxvt/s1BHsszlod8Qqv8CVVA==",
+            serviceKey = BuildConfig.API_KEY,
             hospitalName = hospitalName,
             pageNo = pageNo.toString(),
             latitude = latitude.toString(),
---
+++ app/src/main/java/com/example/lifesemantics/util/PagingSource.kt
@@ -0,0 +1,44 @@
+package com.example.lifesemantics.util
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.lifesemantics.BuildConfig
+import com.example.lifesemantics.data.api.HospitalService
+import com.example.lifesemantics.data.entity.Item
+import javax.inject.Inject
+
+class PagingSource @Inject constructor(
+    private val service: HospitalService,
+    private val hospitalName: String,
+    private val latitude: Double,
+    private val longitude: Double
+) : PagingSource<Int, Item>() {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Item> {
+        val page = params.key ?: 1
+        return try {
+            val response = service.getHospitalInfo(
+                serviceKey = BuildConfig.API_KEY,
+                hospitalName = hospitalName,
+                pageNo = page.toString(),
+                latitude = latitude.toString(),
+                longitude = longitude.toString(),
+                radius = "5000"
+            )
+
+            val items = response.body?.items?.itemList
+            LoadResult.Page(
+                data = items!!,
+                prevKey = if (page == 0) null else page - 1,
+                nextKey = page + 1
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+    override fun getRefreshKey(state: PagingState<Int, Item>): Int? {
+        return state.anchorPosition?.let {
+            state.closestPageToPosition(it)?.prevKey
+        }
+    }
+
+}
